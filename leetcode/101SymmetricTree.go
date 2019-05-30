c9665a4a4ebb055ee334eeb841a1b75dc42155d3
Gaozhen Ying <yinggaozhen@hotmail.com>
2019-05-30 19:09:08 +0800

    ADD : leetcode 101SymmetricTree

---
+++ leetcode/101SymmetricTree.go
@@ -0,0 +1,37 @@
+package main
+
+func main() {
+
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	return _isSymmetric(root.Left, root.Right)
+}
+
+func _isSymmetric(left *TreeNode, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+
+	if left == nil || right == nil {
+		return false
+	}
+
+	if left.Val != right.Val {
+		return false
+	}
+
+	return _isSymmetric(left.Left, right.Right) && _isSymmetric(left.Right, right.Left)
+}
