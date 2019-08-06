210dacfa698f015c5c3407164d2501acfe87433a
didi <yinggaozhen@hotmail.com>
2019-08-06 19:35:47 +0800

    add interface extends

--- struct/struct_type_interface_test.go
+++ struct/struct_type_interface_test.go
@@ -1,27 +1,29 @@
 package test
 
 import (
+	"fmt"
 	"testing"
 )
 
 type IRoute interface {
-	GET() IRoute
-	POST() IRoute
+	GET()
+	POST()
 }
 
 type Router struct {
 }
 
-func (r Router) GET() IRoute {
-	panic("implement me")
+var _ IRoute = Router{}
+
+func (r Router) GET() {
+	fmt.Println("GET implement me")
 }
 
-func (r Router) POST() IRoute {
-	panic("implement me")
+func (r Router) POST() {
+	fmt.Println("POST implement me")
 }
-
-var _ IRoute = &Router{}
 
 func TestTypeInterface(t *testing.T) {
-
+	router := &Router{}
+	router.GET()
 }
