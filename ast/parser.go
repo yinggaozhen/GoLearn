3fdd3686d648bfac3d9790282d66cc08be423de8
Gaozhen Ying <yinggaozhen@hotmail.com>
2019-09-30 16:09:56 +0800

    读取文件还是以源文件路径为准

--- ast/parser.go
+++ ast/parser.go
@@ -1,16 +1,15 @@
-package ast
+package main
 
 import (
 	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
-	"testing"
 )
 
 // 参考资料 : http://www.01happy.com/golang-ast-use/
 // 获取注释
-func TestParseComment(t *testing.T) {
+func main() {
 	parseFile := "/data/github/GoLearn/ast/controller/hello_comment.go"
 
 	fset := token.NewFileSet()
---
+++ build/main1/index.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+)
+
+// go build -o /tmp/hello index.go && /tmp/hello
+// 最终获取到的结果还是不变，读文件也是
+
+func main() {
+	fmt.Println(filepath.Abs("."))
+	fmt.Println(filepath.Abs("../"))
+
+	result, _ := ioutil.ReadFile("../hello")
+	fmt.Println(string(result))
+}
