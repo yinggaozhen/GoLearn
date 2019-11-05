a19f2694244a1e4acf1a0fb134d23c1d42aca46c
Gaozhen Ying <yinggaozhen@hotmail.com>
2019-11-05 15:08:02 +0800

    add goroutines block

---
+++ channel/make_channel_block_test.go
@@ -0,0 +1,28 @@
+package array
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestChannelGoBlock(t *testing.T) {
+	c := make(chan string, 2)
+
+	go func() {
+		// goroutines block
+		for {
+			r := <-c
+			fmt.Println(r)
+		}
+	}()
+
+	go func() {
+		for {
+			time.Sleep(3 * time.Second)
+			c <- "hello"
+		}
+	}()
+
+	time.Sleep(200 * time.Second)
+}
