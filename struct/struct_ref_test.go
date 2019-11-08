e7cac644a9f5149183969931f66f7375eea349a0
Gaozhen Ying <yinggaozhen@hotmail.com>
2019-11-08 11:23:34 +0800

    fix

--- struct/struct_ref_test.go
+++ struct/struct_ref_test.go
@@ -11,13 +11,19 @@ type students struct {
 
 func (s *students) add(name string) {
 	s.peoples = append(s.peoples, name)
+}
 
+func (s students) add2(name string) {
+	s.peoples = append(s.peoples, name)
 }
 
 func TestAppendStudent(t *testing.T) {
 	s := students{}
 	s.add("jack")
 	s.add("tom")
+	fmt.Println(s)
 
+	s.add2("jack2")
+	s.add2("tom2")
 	fmt.Println(s)
 }
