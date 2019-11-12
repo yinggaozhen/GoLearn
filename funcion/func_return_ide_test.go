45f8b8f870d4c0979e029487468f1a6caf65a009
Gaozhen Ying <yinggaozhen@hotmail.com>
2019-11-12 16:44:42 +0800

    add func ide

---
+++ funcion/func_return_ide_test.go
@@ -0,0 +1,15 @@
+package array
+
+import (
+	"errors"
+	"testing"
+)
+
+// Map Push
+func TestFuncIDE(t *testing.T) {
+	_ = helloIDE()
+}
+
+func helloIDE() error {
+	return errors.New("hello")
+}
--- mystring/fmt_string_test.go
+++ mystring/fmt_string_test.go
@@ -1,25 +1,9 @@
 package array
 
 import (
-	"bytes"
 	"fmt"
-	"testing"
 )
 
-func TestFmt(t *testing.T) {
-	w := bytes.Buffer{}
-	fmt.Fprint(&w, 1)
-	fmt.Println(w.String())
-
-	w.Reset()
-	fmt.Fprint(&w, 1.222)
-	fmt.Println(w.String())
-
-	w.Reset()
-	fmt.Fprint(&w, 16564894879)
-	fmt.Println(w.String())
-
-	w.Reset()
-	fmt.Fprint(&w, "hello world")
-	fmt.Println(w.String())
+func main() {
+	fmt.Println("hello")
 }
