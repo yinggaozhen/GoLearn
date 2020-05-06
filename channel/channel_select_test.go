6bc396ee81cb4f64f4ba097c63d39a13c179698a
Gaozhen Ying <yinggaozhen@hotmail.com>
2020-05-06 20:07:18 +0800

    fix

--- channel/channel_select_test.go
+++ channel/channel_select_test.go
@@ -2,6 +2,7 @@ package array
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 )
 
@@ -38,11 +39,34 @@ func TestChannelSelectNormal(t *testing.T) {
 
 	select {
 	// deadlock
-	case <-channel:
-		fmt.Println("channel output")
+	case r := <-channel:
+		fmt.Println("channel output : " + strconv.Itoa(r))
 	default:
 		fmt.Println("default")
 	}
 
 	fmt.Println("select finish")
+}
+
+// 测试channel range
+func TestChannelRange(t *testing.T) {
+	channel := make(chan int, 100)
+	channel <- 1
+	channel <- 2
+	channel <- 3
+
+	// close(channel) // 如果不加上这一句，后边range channel会出现死锁
+
+	fmt.Println(len(channel)) // 3
+
+	for cr := range channel {
+		l := len(channel)
+		fmt.Println("channel output : " + strconv.Itoa(cr))
+		fmt.Println("channel len : " + strconv.Itoa(l))
+
+		// 如果不明确channel是否关闭，则需要加一个len
+		// if l == 0 { break }
+	}
+
+	fmt.Println("range finish")
 }
